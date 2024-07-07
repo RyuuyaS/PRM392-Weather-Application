8ef5886d2173f60790917004cc111f8f9e51b74b
RyuuyaS <thinhyolo1994@gmail.com>
2024-07-07 23:36:40 +0700

    Add onrequest at base

--- app/src/main/java/com/example/weather_application/ui/activity/BaseActivity.java
+++ app/src/main/java/com/example/weather_application/ui/activity/BaseActivity.java
@@ -1,9 +1,11 @@
 package com.example.weather_application.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +14,7 @@ import com.example.weather_application.utils.MyApplication;
 
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +35,13 @@ public class BaseActivity extends AppCompatActivity {
       overrideConfiguration.uiMode = uiMode;
     }
     super.applyOverrideConfiguration(getResources().getConfiguration());
+  }
+
+    public abstract void onRequestPermisisonsResult(int requestCode, @NonNull String[] permisisons, @NonNull int[] grantResults);
+
+  @SuppressLint("MissingSuperCall")
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    onRequestPermisisonsResult(requestCode, permissions, grantResults);
   }
 }
