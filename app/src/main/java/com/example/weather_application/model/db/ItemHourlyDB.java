a6bc2ac6667a337dc2080e9015a5a6a88f1d9292
Khanh Huyen Nguyen <102915743+hoenntk68@users.noreply.github.com>
2024-07-16 23:39:59 +0700

    fix: 4 days item

--- app/src/main/java/com/example/weather_application/model/db/ItemHourlyDB.java
+++ app/src/main/java/com/example/weather_application/model/db/ItemHourlyDB.java
@@ -23,7 +23,7 @@ import io.objectbox.annotation.Id;
 public class ItemHourlyDB extends AbstractItem<ItemHourlyDB, ItemHourlyDB.MyViewHolder> {
   @Id
   private long id;
-  private long fiveDayWeatherId;
+  private long fourdays;
   private int dt;
   private double temp;
   private int weatherCode;
